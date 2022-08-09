53d4e0b134c9e55249c39f20b5a84127d2a129e5
BILEL SALEM <70206023+bilelsalem2020@users.noreply.github.com>
2022-08-09 11:26:05 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
+import useGame from "./useGame";
 function App() {
+  const {
+    text,
+    timeRemaining,
+    isTimeRunning,
+    countWord,
+    handleChange,
+    startGame,
+    textareaRef,
+  } = useGame(10);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>TYPING GAME</h1>
+      <textarea
+        value={text}
+        onChange={handleChange}
+        disabled={!isTimeRunning}
+        ref={textareaRef}
+      />
+      <h4>time Remaining : {timeRemaining}</h4>
+      <button disabled={isTimeRunning} onClick={startGame}>
+        {" "}
+        START
+      </button>
+      <h1>count Word : {countWord}</h1>
     </div>
   );
 }
 
 export default App;
+
